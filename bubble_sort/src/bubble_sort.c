d974f8968371c32d77f1fdf5e2768417573827fd
Enes Kaya <ekaya18@ku.edu.tr>
2023-02-22 14:56:50 +0300

    bubble sort tamamlandı

---
+++ bubble_sort/src/bubble_sort.c
@@ -0,0 +1,51 @@
+/*
+ ============================================================================
+ Name        : bubble_sort.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubblesort(int arr[], int len);
+
+int main(void) {
+	// test array:
+	int array[] = {24, 14, 15, 7, 4, 27, 1, 8};
+	int len = sizeof(array)/sizeof(array[0]);
+
+	bubblesort(array, len);
+
+	return EXIT_SUCCESS;
+}
+
+
+void bubblesort(int arr[], int len) {
+
+	int temp, i, j, k;
+
+	for(i=0; i<len; i++)
+	{
+		int ordered = 1;			// to optimize the time complexity of the algorithm
+		for(j=len-1; j>i; j--)
+		{
+			if(arr[j-1] > arr[j])
+			{
+				ordered = 0;
+				temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		if(ordered)
+			break;
+	}
+	for(k=0; k<len; k++)
+		{
+			printf("%d ", arr[k]);
+	}
+}
