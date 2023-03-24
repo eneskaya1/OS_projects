b342e1cafbae3ea2a9d6f0e5018bc671a83a7c20
Enes Kaya <ekaya18@ku.edu.tr>
2023-03-24 12:41:15 +0300

    Threadler kullanarak dosya yazma okuma işlemleri yapıldı.

---
+++ thread_functions2/src/thread_functions2.c
@@ -0,0 +1,79 @@
+/*
+ ============================================================================
+ Name        : thread_functions2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define TOTAL 50000000
+#define BUFSIZE 15
+
+struct textArgs {
+    char fileName[20];
+    char text[20];
+};
+typedef struct textArgs TextArgs;
+
+pthread_t fileWrite[2];
+pthread_t fileRead;
+char *test[TOTAL];
+
+void* writing(void *args) {
+    TextArgs *TextArgsPtr = args;
+    char *fileName = TextArgsPtr->fileName;
+    char *text = TextArgsPtr->text;
+    FILE *fp;
+    fp = fopen(fileName, "w");
+    fputs(text, fp);
+
+    fclose(fp);
+    pthread_exit(0);
+}
+
+void* reading(void *arg) {
+    char *fileName = (char *)arg;
+    FILE *fp;
+    fp = fopen(fileName, "r");
+    int i = 0;
+    test[i] = malloc(BUFSIZE);
+    while(fgets(test[i], BUFSIZE, fp) != NULL) {
+      strtok(test[i],"\n");
+      i++;
+      test[i] = malloc(BUFSIZE);
+      if (i == TOTAL){
+         break;
+      }
+   }
+
+    fclose(fp);
+    pthread_exit(0);
+}
+
+int main(void)
+{
+    TextArgs thread1Args;
+    sprintf(thread1Args.fileName, "file.txt");
+    sprintf(thread1Args.text, "Thread 1");
+    TextArgs thread2Args;
+    sprintf(thread2Args.fileName, "file.txt");
+    sprintf(thread2Args.text, "Thread 2");
+
+    pthread_create(&fileWrite[0], NULL, writing, (void *)&thread1Args);
+    pthread_create(&fileWrite[1], NULL, writing, (void *)&thread2Args);
+
+    pthread_create(&fileRead, NULL, reading, "file.txt");
+    pthread_join(fileRead, NULL);
+
+
+
+    return 0;
+}
